6f685ad62d78f46d0891e280d6f7f8b5bcf54168
mellsh <mellsh0813@gmail.com>
2025-03-24 15:43:27 +0900

    create_comments_dbtable

--- db_init.js
+++ db_init.js
@@ -5,6 +5,7 @@ const db = new sqlite3.Database('./database.db');
 
 // 테이블 준비 함수
 function initDB() {
+  // articles 테이블 생성
   db.run(`
     CREATE TABLE IF NOT EXISTS articles (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,6 +18,23 @@ function initDB() {
       console.error("테이블 생성 에러:", err);
     } else {
       console.log("테이블 준비 완료(articles)");
+    }
+  });
+
+  // comments 테이블 생성
+  db.run(`
+    CREATE TABLE IF NOT EXISTS comments (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      content TEXT,
+      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+      article_id INTEGER,
+      FOREIGN KEY (article_id) REFERENCES articles(id) ON DELETE CASCADE
+    )
+  `, (err) => {
+    if (err) {
+      console.error("테이블 생성 에러:", err);
+    } else {
+      console.log("테이블 준비 완료(comments)");
     }
   });
 }
